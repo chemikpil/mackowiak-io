4bc62df610427485b46188f4864ac58998affacd
Michał Maćkowiak <chemikpil@gmail.com>
2025-12-27 12:54:12 +0100

    fix: 🚀 update vite configuration for github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,12 +17,9 @@ export default defineConfig({
 		// @ts-ignore
 		plugins: [tailwindcss()],
 		resolve: {
-			alias: [
-				{
-					find: "@",
-					replacement: path.resolve(__dirname, "./src"),
-				},
-			],
+			alias: {
+				"@": path.resolve(process.cwd(), "src"),
+			},
 		},
 	},
 });
