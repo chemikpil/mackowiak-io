618438b420fb27312540e9f977d92f7ee538fe7e
Michał Maćkowiak <chemikpil@gmail.com>
2026-03-09 08:15:11 +0100

    feat: 🐞 create default 404 page

--- src/components/shared/logo/logo.style.ts
+++ src/components/shared/logo/logo.style.ts
@@ -2,11 +2,12 @@ import type { VariantProps } from "tailwind-variants";
 import { tv } from "tailwind-variants";
 
 export const logoVariants = tv({
-  base: "",
+  base: "**:data-[slot=broken-glasses]:hidden",
   variants: {
     variant: {
       default: {},
       ui: "[--glasses-stop-1:oklch(0.659_0.188_250.769)] [--glasses-stop-2:oklch(0.655_0.213_321.332)] [--glasses-stop-3:oklch(0.65_0.238_18.289)] [--glasses-stop-4:oklch(0.757_0.177_59.992)]",
+      broken: "**:data-[slot=glasses]:hidden **:data-[slot=broken-glasses]:block",
     },
   },
 });
