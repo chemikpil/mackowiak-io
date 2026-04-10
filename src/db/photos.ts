a0d368fc95cc966a094a12b11f09c2f5411fa9d5
Michał Maćkowiak <chemikpil@gmail.com>
2026-04-10 07:30:58 +0200

    feat: 🖼️ add another photo from Madeira

--- src/db/photos.ts
+++ src/db/photos.ts
@@ -1,6 +1,12 @@
 export const slides = [
 	[
 		{
+			id: 11,
+			name: "Madeira, Portugal, Petridge, Part II",
+			aspectRatio: "4/5",
+			src: "/photography/2021-madeira-petridge-1.jpg",
+		},
+		{
 			id: 10,
 			name: "Cais das Colunas, Lisbon, Portugal",
 			aspectRatio: "17/10",
@@ -24,14 +30,14 @@ export const slides = [
 			aspectRatio: "4/5",
 			src: "/photography/2020-tatry-2.jpg",
 		},
+	],
+	[
 		{
 			id: 6,
 			name: "Tatry, Poland, Part I",
 			aspectRatio: "4/5",
 			src: "/photography/2020-tatry-1.jpg",
 		},
-	],
-	[
 		{
 			id: 5,
 			name: "Poznań, Poland, Strajk kobet",
